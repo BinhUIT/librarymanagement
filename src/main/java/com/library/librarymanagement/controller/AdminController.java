6bd779cff1ea260caa7692dd9ce4407d10151870
BinhUIT <22520128@gm.uit.edu.vn>
2024-12-15 19:55:30 +0700

    Add some admin features

--- src/main/java/com/library/librarymanagement/controller/AdminController.java
+++ src/main/java/com/library/librarymanagement/controller/AdminController.java
@@ -1,13 +1,21 @@
 package com.library.librarymanagement.controller;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.library.librarymanagement.entity.User;
+import com.library.librarymanagement.request.RegisterRequest;
 import com.library.librarymanagement.security.TokenSecurity;
 import com.library.librarymanagement.service.AdminService;
 
@@ -30,6 +38,47 @@ public class AdminController {
 
         return adminService.lockOrUnlockLibrarian(id, isUnlock);
         
+    } 
+
+    @GetMapping("/admin/getAllLinrarian") 
+    public List<User> getAllLibrarian(@RequestHeader("Authorization") String authHeader) 
+    {
+        if(authHeader==null||!authHeader.startsWith("Bearer ") || !tokenSecurity.checkToken(authHeader.substring(7))||tokenSecurity.extractRole(authHeader)<=1) 
+        {
+            return null;
+        } 
+        return adminService.getAllLibrarian();
+    } 
+
+    @PostMapping("/admin/createLibrarian") 
+    public ResponseEntity<User> createLibrarian(@RequestHeader("Authorization") String authHeader, @RequestBody RegisterRequest request) 
+    {
+        
+        if(authHeader==null||!authHeader.startsWith("Bearer ") || !tokenSecurity.checkToken(authHeader.substring(7))||tokenSecurity.extractRole(authHeader)<=1) 
+        {
+            return null;
+        }  
+        return adminService.createLibrarian(request);
+    }  
+
+    @PutMapping("/admin/lock/librarian/{id}") 
+    public ResponseEntity<String> lockLibrarian(@RequestHeader("Authorization") String authHeader, @PathVariable int id) 
+    {
+        if(authHeader==null||!authHeader.startsWith("Bearer ") || !tokenSecurity.checkToken(authHeader.substring(7))||tokenSecurity.extractRole(authHeader)<=1) 
+        {
+            return null;
+        } 
+        return adminService.lockLibrarian(id);
+    } 
+
+    @PutMapping("/admin/unlock/librarian/{id}") 
+    public ResponseEntity<String> unlockLibrarian(@RequestHeader("Authorization") String authHeader, @PathVariable int id) 
+    {
+        if(authHeader==null||!authHeader.startsWith("Bearer ") || !tokenSecurity.checkToken(authHeader.substring(7))||tokenSecurity.extractRole(authHeader)<=1) 
+        {
+            return null;
+        } 
+        return adminService.unlockLibrarian(id);
     }
 
 }
--- src/main/java/com/library/librarymanagement/service/AdminService.java
+++ src/main/java/com/library/librarymanagement/service/AdminService.java
@@ -1,13 +1,19 @@
 package com.library.librarymanagement.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import com.library.librarymanagement.entity.User;
 import com.library.librarymanagement.repository.UserRepository;
+import com.library.librarymanagement.request.RegisterRequest;
 import com.library.librarymanagement.security.TokenSecurity;
+import com.library.librarymanagement.securitySalt.BcryptSalt;
 
 @Service
 public class AdminService { 
@@ -15,7 +21,9 @@ public class AdminService {
     private UserRepository userRepo; 
 
     @Autowired 
-    private TokenSecurity tokenSecurity; 
+    private TokenSecurity tokenSecurity;  
+    @Autowired 
+    private BcryptSalt bcryptSalt;
 
     public ResponseEntity<String> lockOrUnlockLibrarian(int librarianId, boolean lockOrUnlock) 
     {  
@@ -41,6 +49,66 @@ public class AdminService {
             return new ResponseEntity<>("Librarian not found", HttpStatus.NOT_FOUND);
         } 
         return null;
+    }
+
+    public List<User> getAllLibrarian() 
+    {
+        List<User> listRes= new ArrayList<>();
+        List<User> listUser= userRepo.findAll();
+        for(int i=0;i<listUser.size();i++) 
+        {
+            if(listUser.get(i).getRole()==1&&listUser.get(i).getEnable()==true) 
+            {
+                listRes.add(listUser.get(i));
+            }
+        } 
+        return listRes;
+    }
+
+    public ResponseEntity<User> createLibrarian(RegisterRequest request) 
+    {
+        User user= userRepo.findByFullName(request.getFullName());
+        if(user!=null) 
+        {
+            return new ResponseEntity<>(null, HttpStatus.ALREADY_REPORTED);
+        } 
+        user= userRepo.findByEmail(request.getEmail());
+        if(user!=null) 
+        {
+            return new ResponseEntity<>(null, HttpStatus.ALREADY_REPORTED);
+        } 
+        User newUser= new User(request);
+        int newId= userRepo.findAll().size();
+        newUser.setUserId(newId);
+        newUser.setPassword(BCrypt.hashpw(request.getPassword(), bcryptSalt.getSalt())); 
+        newUser.setRole(1);
+        userRepo.save(newUser);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> lockLibrarian(int librarianId) 
+    {
+        User librarian= userRepo.findById(librarianId).orElse(null); 
+        if(librarian==null||librarian.getEnable()==false||librarian.getRole()<1) 
+        {
+            return new ResponseEntity<>("Can not delete", HttpStatus.BAD_REQUEST);
+        } 
+        librarian.setEnable(false);
+        userRepo.save(librarian);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    } 
+
+
+    public ResponseEntity<String> unlockLibrarian(int librarianId)
+    {
+        User librarian= userRepo.findById(librarianId).orElse(null); 
+        if(librarian==null||librarian.getEnable()==true||librarian.getRole()<1) 
+        {
+            return new ResponseEntity<>("Can not unlock", HttpStatus.BAD_REQUEST);
+        } 
+        librarian.setEnable(true);
+        userRepo.save(librarian);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
     
 }
